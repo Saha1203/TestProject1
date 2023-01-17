5f2263fa15fe8991048e9cbf6f159700adbd06e5
Rahul Saha <rahulsaha.wbnxb@gmail.com>
2023-01-17 15:39:21 +0530

    #Features are added

--- Test1.py
+++ Test1.py
@@ -1,1 +1,15 @@
-print("Hello")
+import os
+List1 = [8,9,3,6,1,10]
+List1.reverse()
+print("The reverse list is",List1)
+
+List2 = [41,56,12,33,7,98,77]
+List2.sort()
+print("The sorted list is",List2)
+
+List3 = []
+List3 = List1.copy()
+print("List3 = ",List3)
+
+Indexvalue = List2[2:6]
+print("Index values are", Indexvalue)
